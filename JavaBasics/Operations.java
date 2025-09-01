ca5504f6bb1fd5155c29c39eef7b6fdca71109d7
ParavinMulla786 <paravinmulla2004@gmail.com>
2025-09-01 18:14:15 +0530

    basic arithmetic operations in Java

---
+++ JavaBasics/Operations.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Operations {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int addition = a+ b;
+        int substraction = a- b;
+        int multiplication = a* b;
+        int division  = a/ b;
+        int remainder = a%b;
+        System.out.println(addition);
+        System.out.println(substraction);
+        System.out.println(multiplication);
+        System.out.println(division);
+        System.out.println(remainder);
+
+
+
+
+    }
+}
